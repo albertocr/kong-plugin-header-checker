ac5fb24d75907c995859b065386762d2deb3079f
Alberto Cuesta <acuesta@das-nano.com>
2019-06-25 13:16:54 +0200

    Change header content matching from contains to starts with

--- handler.lua
+++ handler.lua
@@ -10,14 +10,17 @@ function HeaderCheckerHandler:new()
   HeaderCheckerHandler.super.new(self, "header-checker")
 end
 
+local function starts_with(str, start)
+  return str:sub(1, #start) == start
+end
+
 function HeaderCheckerHandler:access(conf)
   HeaderCheckerHandler.super.access(self)
   local req_headers = kong.request.get_headers()
   local req_header_to_check = req_headers[conf.header_to_check]
-
   allowed = false
   for i = 1, table.maxn(conf.allowed_header_values) do
-    if string.match(conf.allowed_header_values[i], req_header_to_check) then
+    if starts_with(req_header_to_check, conf.allowed_header_values[i]) then
       allowed = true
     end
   end
