055c7ce990af6e4de5de73e65afa20021079a89b
Alberto Cuesta <acuesta@das-nano.com>
2019-09-25 15:07:06 +0200

    Adapt to KONG version 1.3

--- handler.lua
+++ handler.lua
@@ -1,7 +1,7 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local responses = require "kong.tools.responses"
-
+local responses = kong.response
 local HeaderCheckerHandler = BasePlugin:extend()
+local kong = kong
 
 HeaderCheckerHandler.PRIORITY = 1000
 HeaderCheckerHandler.VERSION = "0.1.0"
@@ -18,14 +18,15 @@ function HeaderCheckerHandler:access(conf)
   HeaderCheckerHandler.super.access(self)
   local req_headers = kong.request.get_headers()
   local req_header_to_check = req_headers[conf.header_to_check]
-  allowed = false
+  local allowed = false
   for i = 1, table.maxn(conf.allowed_header_values) do
     if starts_with(req_header_to_check, conf.allowed_header_values[i]) then
       allowed = true
     end
   end
   if not allowed then
-    return responses.send_HTTP_FORBIDDEN()
+    kong.log.err("Missing required configuration for accessing this route")
+    return kong.response.exit(403, "Forbidden")
   end
 end
 
